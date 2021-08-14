71774f99ef61addc4a2e569178771b5b20eef455
Evrimbuba <61472420+Evrimbuba@users.noreply.github.com>
2021-08-14 20:12:46 +0300

    Projeyi Ekleme

---
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    internal class CourseDto
+    {
+        public string CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CategoryId { get; set; }
+        public FeatureDto Feature { get; set; }
+        public CategoryDto Category { get; set; }
+    }
+}
